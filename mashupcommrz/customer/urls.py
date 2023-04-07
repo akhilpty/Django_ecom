af8911452aaa91f2a56ac75326718fd29c0861b3
akhilpty <akhilpattazhy1998@gmail.com>
2023-04-07 10:08:42 +0530

    first commit

---
+++ mashupcommrz/customer/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from . import views
+from django.views.generic import RedirectView
+
+urlpatterns = [
+     path('registercustomer', views.registercustomer, name='registercustomer'),
+     path('logout', views.logoutcustomer, name='logoutcustomer'),
+     path('logincustomer', views.logincustomer, name='logincustomer'),
+     path('', views.logincustomer, name='logincustomer'),
+     path('products', views.homepage, name='products'),
+     path('addtocart', views.addproducttocart, name='addtocart'),
+     path('removefromcart', views.removeproductfromcart, name='removefromcart'),
+     path('viewcustomercart', views.viewcustomercart, name='viewcustomercart'),
+     path('removefromcartpage/', views.removeproductcartpage, name='removeproductcartpage'),
+     path('checkoutcustomer', views.checkoutcustomer, name='checkoutcustomer'),
+     path('markpaymentsuccess', views.markpaymentsuccess, name='markpaymentsuccess'),
+     ]
